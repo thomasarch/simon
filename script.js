2e8aa026e729f906003517ca7ed399d391775b6e
Thomas DeFelice <thomasarch@gmail.com>
2019-04-10 06:04:06 -0400

    sound is working

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const pieces = document.querySelectorAll('.piece');
 const gameContainer = document.querySelector('.game-container');
+const audioContext = new AudioContext();
+const startButton = document.querySelector('#start-button');
+const tones = [164.813, 440, 277.183, 329.628]
 let sequence = [];
 let step = 0;
 let acceptUserInput = false;
@@ -8,9 +11,16 @@ function randomNum(min, max) {
 	return Math.floor(Math.random() * ((max + 1) - min) + min);
 }
 
+function startGame() {
+	console.log('starting!');
+	audioContext.resume();
+	addToSequence();	
+}
+
 function flashPiece(number) {
 	let piece = pieces[number];
 	piece.classList.add('bright');
+	playFrequency(tones[number]);
 	setTimeout(() => { piece.classList.remove('bright') }, 200);
 }
 
@@ -52,6 +62,7 @@ function gameover() {
 }
 
 function pressedPiece() {
+	// playFrequency(440);
 	if (acceptUserInput == true) {
 		flashPiece(this.dataset.number);
 		if (this.dataset.number == sequence[step]) {
@@ -68,10 +79,24 @@ function pressedPiece() {
 	}
 }
 
+function playFrequency(frequency) {
+	const beep = audioContext.createOscillator();
+	const gain = audioContext.createGain();
+	beep.connect(gain);
+	gain.connect(audioContext.destination);
+	gain.gain.setValueAtTime(0.05, audioContext.currentTime);
+	beep.frequency.value = frequency
+	beep.start(0);
+	setTimeout(() => { 
+		gain.gain.exponentialRampToValueAtTime(0.00001, audioContext.currentTime + 0.04)
+	}, 250)
+
+}
+
+
+startButton.addEventListener('click', startGame);
+
 pieces.forEach(piece => {
 	piece.classList.toggle('wait-for-sequence')
 	piece.addEventListener('click', pressedPiece)
 });
-
-addToSequence();
-
